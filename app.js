723058b49e6b3a3bd377b21ffa489877985f4f36
xuqil <13829842679@163.com>
2018-12-07 14:27:49 +0800

    点击事件

--- app.js
+++ app.js
@@ -2,19 +2,33 @@
 new Vue({
     el:"#vue-app",
     data:{
-        name:"小明",
-        website: "https://www.baidu.com",
-        websiteTag : "<a href='https://www.baidu.com'>百度一下</a>"
+       age:30,
+        x:0,
+        y:0,
     },
     methods:{ //创建方法表
-        greet: function (time) {  // 定义greet方法
-            return 'Good ' + time + " " + this.name + "!";
+        add:function () {
+            this.age++;
+        },
+        subtract:function () {
+            this.age --;
+        },
+        adc:function (inc) {
+            this.age += inc;
+        },
+        subc:function (dec) {
+            this.age -= dec;
+        },
+        updateXY: function (envent) {
+           this.x = envent.offsetX,
+               this.y = envent.offsetY
         }
-    }
+        }
 });
 
 /*
 * el:element需要获取的元素，一定是html中的根容器元素
 * data：用于数据的存储
 * method：用于存储各种方法
+* data-binding：给属性绑定值
  */
