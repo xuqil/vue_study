9f45387f64f85b9fd76643a88e1ac232c3261b30
xuqil <13829842679@163.com>
2018-12-07 13:54:09 +0800

    属性绑定

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 new Vue({
     el:"#vue-app",
     data:{
-        name:"小明"
+        name:"小明",
+        website: "https://www.baidu.com",
+        websiteTag : "<a href='https://www.baidu.com'>百度一下</a>"
     },
     methods:{ //创建方法表
         greet: function (time) {  // 定义greet方法
@@ -14,4 +16,5 @@ new Vue({
 /*
 * el:element需要获取的元素，一定是html中的根容器元素
 * data：用于数据的存储
+* method：用于存储各种方法
  */
